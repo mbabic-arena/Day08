1edbe795bd9c7b828498c2a296187ca0331f8497
mbabic-arena <m_babic@teol.net>
2021-03-16 10:42:12 +0100

    Updated exercises

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -2,7 +2,7 @@
 function randomRangeNumber(minNumber, maxNumber) {
   return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
-randomRangeNumber();
+randomRangeNumber(4, 30);
 console.log(randomRangeNumber(4, 40));
 // Only change code above this line
 module.exports = randomRangeNumber;
